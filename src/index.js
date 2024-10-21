7018b2be270354f00508d891cebdc2d928a490ba
the-kumar-amit07 <amitkumarmarndi37@gmail.com>
2024-10-21 10:51:55 +0530

    configuration using CORS and cookieParser

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // require('dotenv').config({path:'./env'})
+import app from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv"
 
@@ -9,6 +10,17 @@ dotenv.config({
 })
 
 connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.log(`Connection to Server Failed:`,error)
+        })
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at PORT::${process.env.PORT}`)
+        })
+    })
+    .catch((error) => {
+        console.log(`MONGODB-CONNECTION-ERROR`,error)
+    })
 
 /* Approach One
 import express from "express"
